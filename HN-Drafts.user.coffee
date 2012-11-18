1826f8028a269a9c3fd5d3d343e9fda54dc85c16
Sean Lang <slang800@gmail.com>
2012-11-17 23:18:48 -0600

    added a fancy draft manager to the account page

--- HN-Drafts.user.coffee
+++ HN-Drafts.user.coffee
@@ -5,32 +5,70 @@
 // @description Save to drafts button for HN
 // @include     http://news.ycombinator.com/reply*
 // @include     http://news.ycombinator.com/item*
+// @include     http://news.ycombinator.com/user*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @version     1
 // ==/UserScript==
 ###
 
-for url of localStorage
-	console.log url
-	$("a[href=\"#{url}\"]").after('<span style="color:red"> (draft saved)</span>')
+switch window.location.pathname
+	when '/user'
+		$('form input[type="submit"]').after("""
+			<hr>
+			<p>drafts:</p>
+			<table id="draft_table"></table>
+		""")
+
+		generate_table = () ->
+			table_html = ''
+			for key, value of localStorage
+				table_html += """
+					<tr>
+						<td>
+							<a href="#{key}">#{value}</a>
+						</td>
+						<td>
+							<input type="button" value="Delete">
+						</td>
+					</tr>
+				"""
+			if table_html is '' then table_html = 'none'
+
+			$('#draft_table').html(table_html)
 
 
-add_comment = $('form input[type="submit"]')
-save_draft = $('form input[type="button"]')
-textarea = $('textarea')[0]
+		generate_table()
 
-add_comment.after('<input type="button" value="save draft">')
+		$('input[value="Delete"]').click( ->
+			console.log $(this).parent().parent().find('a').attr('href')
+			localStorage.removeItem(
+				$(this).parent().parent().find('a').attr('href')
+			)
+			generate_table()
+		)
 
-key = (window.location.pathname + window.location.search).substr(1) # cut off slash at beginning of str
+	else
+		for url of localStorage
+			console.log url
+			$("a[href=\"#{url}\"]").after('<span style="color:red"> (draft saved)</span>')
 
 
-if localStorage.getItem(key)?
-	textarea.value = localStorage.getItem(key)
+		add_comment = $('form input[type="submit"]')
+		save_draft = $('form input[type="button"]')
+		textarea = $('textarea')[0]
 
-save_draft.click( ->
-	localStorage.setItem(key, textarea.value)
-)
+		add_comment.after('<input type="button" value="save draft">')
 
-add_comment.click( ->
-	localStorage.removeItem(key)
-)
+		key = (window.location.pathname + window.location.search).substr(1) # cut off slash at beginning of str
+
+
+		if localStorage.getItem(key)?
+			textarea.value = localStorage.getItem(key)
+
+		save_draft.click( ->
+			localStorage.setItem(key, textarea.value)
+		)
+
+		add_comment.click( ->
+			localStorage.removeItem(key)
+		)
--- HN-Drafts.user.js
+++ HN-Drafts.user.js
@@ -6,6 +6,7 @@
 // @description Save to drafts button for HN
 // @include     http://news.ycombinator.com/reply*
 // @include     http://news.ycombinator.com/item*
+// @include     http://news.ycombinator.com/user*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @version     1
 // ==/UserScript==
@@ -13,33 +14,49 @@
 
 
 (function() {
-  var add_comment, key, save_draft, textarea, url;
+  var add_comment, generate_table, key, save_draft, textarea, url;
 
-  for (url in localStorage) {
-    console.log(url);
-    $("a[href=\"" + url + "\"]").after('<span style="color:red"> (draft saved)</span>');
+  switch (window.location.pathname) {
+    case '/user':
+      $('form input[type="submit"]').after("<hr>\n<p>drafts:</p>\n<table id=\"draft_table\"></table>");
+      generate_table = function() {
+        var key, table_html, value;
+        table_html = '';
+        for (key in localStorage) {
+          value = localStorage[key];
+          table_html += "<tr>\n	<td>\n		<a href=\"" + key + "\">" + value + "</a>\n	</td>\n	<td>\n		<input type=\"button\" value=\"Delete\">\n	</td>\n</tr>";
+        }
+        if (table_html === '') {
+          table_html = 'none';
+        }
+        return $('#draft_table').html(table_html);
+      };
+      generate_table();
+      $('input[value="Delete"]').click(function() {
+        console.log($(this).parent().parent().find('a').attr('href'));
+        localStorage.removeItem($(this).parent().parent().find('a').attr('href'));
+        return generate_table();
+      });
+      break;
+    default:
+      for (url in localStorage) {
+        console.log(url);
+        $("a[href=\"" + url + "\"]").after('<span style="color:red"> (draft saved)</span>');
+      }
+      add_comment = $('form input[type="submit"]');
+      save_draft = $('form input[type="button"]');
+      textarea = $('textarea')[0];
+      add_comment.after('<input type="button" value="save draft">');
+      key = (window.location.pathname + window.location.search).substr(1);
+      if (localStorage.getItem(key) != null) {
+        textarea.value = localStorage.getItem(key);
+      }
+      save_draft.click(function() {
+        return localStorage.setItem(key, textarea.value);
+      });
+      add_comment.click(function() {
+        return localStorage.removeItem(key);
+      });
   }
-
-  add_comment = $('form input[type="submit"]');
-
-  save_draft = $('form input[type="button"]');
-
-  textarea = $('textarea')[0];
-
-  add_comment.after('<input type="button" value="save draft">');
-
-  key = (window.location.pathname + window.location.search).substr(1);
-
-  if (localStorage.getItem(key) != null) {
-    textarea.value = localStorage.getItem(key);
-  }
-
-  save_draft.click(function() {
-    return localStorage.setItem(key, textarea.value);
-  });
-
-  add_comment.click(function() {
-    return localStorage.removeItem(key);
-  });
 
 }).call(this);
